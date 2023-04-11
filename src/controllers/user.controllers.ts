6ad5cb657dc8f91d49304f4a664f8981a3a4bbb3
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-11 15:21:20 -0300

    feat: register usercontroller developed

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import {
+  registerUserService,
+  updateUserService,
+  deleteUserService,
+} from "../services/user";
+
+export const registerUserController = async (req: Request, res: Response) => {
+  const data = req.body;
+  const user = await registerUserService(data);
+
+  return res.status(201).json(user);
+};
+
+export const updateUserController = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const data = req.body;
+  const updated_user = await updateUserService(id, data);
+
+  return res.status(200).json(updated_user);
+};
+
+export const deleteUserController = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  await deleteUserService(id);
+
+  return res.status(204).send();
+};
