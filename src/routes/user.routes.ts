8bd7711339a36455e614e43c6ec4516aeab1c079
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-24 21:28:56 -0300

    feat: retrieve user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,25 @@ import { Router } from "express";
 import {
   deleteUserController,
   registerUserController,
+  retrieveUserController,
   updateUserController,
 } from "../controllers/user.controllers";
+import {
+  verifyDuplicatedCPF,
+  verifyDuplicatedEmail,
+  verifyDuplicatedUsername,
+} from "../middlewares";
+import { authenticationMiddleware } from "../middlewares/authentication.middleware";
 
 export const userRouter = Router();
 
-userRouter.post("/register", registerUserController);
+userRouter.post(
+  "/register",
+  verifyDuplicatedCPF,
+  verifyDuplicatedEmail,
+  verifyDuplicatedUsername,
+  registerUserController
+);
+userRouter.get("/retrieve", authenticationMiddleware, retrieveUserController);
 userRouter.patch("/update", updateUserController);
 userRouter.delete("/destroy", deleteUserController);
