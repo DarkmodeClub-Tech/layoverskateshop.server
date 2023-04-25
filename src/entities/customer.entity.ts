a3c2b2654b567a0ef03d864892f55e637ca330bd
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-25 20:02:19 -0300

    update: address relation removed from customer and assigned to user

--- src/entities/customer.entity.ts
+++ src/entities/customer.entity.ts
@@ -1,17 +1,10 @@
 import { Entity, ManyToOne, OneToMany, OneToOne } from "typeorm";
 import { User } from "./user.entity";
-import { Address } from "./address.entity";
 import { Cart } from "./cart.entity";
 import { Order } from "./order.entity";
 
 @Entity("customers")
 export class Customer extends User {
-  @ManyToOne(() => Address, (address) => address.customers, {
-    eager: true,
-    cascade: true,
-  })
-  address: Address;
-
   @OneToOne(() => Cart, (cart) => cart.customer)
   cart: Cart;
 
