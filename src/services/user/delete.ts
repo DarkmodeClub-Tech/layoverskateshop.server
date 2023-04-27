c210c8eaaea15fbf2b88e2079da0ca9c923dacd2
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-26 23:38:07 -0300

    refactor: switch from user to customer prefixes and variable names

--- src/services/user/delete.ts
+++
@@ -1,8 +0,0 @@
-import AppDataSource from "../../data-source";
-import { Customer } from "../../entities";
-
-export const deleteAccountService = async (id: string) => {
-  const customerRepo = AppDataSource.getRepository(Customer);
-  await customerRepo.delete(id);
-  return;
-};
