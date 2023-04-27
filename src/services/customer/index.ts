e13e9f149c850a550d689a44415290e17d75e7a7
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-26 23:29:45 -0300

    refactor: switch user to customer prefixes and variable names

---
+++ src/services/customer/index.ts
@@ -0,0 +1,13 @@
+import { registerUserService } from "./register";
+import { updateUserService } from "./update";
+import { deleteAccountService } from "./delete";
+import { retrieveUserService } from "./retrieve";
+import { deactivateAccountService } from "./deactivate";
+
+export {
+  registerUserService,
+  updateUserService,
+  deleteAccountService,
+  retrieveUserService,
+  deactivateAccountService,
+};
