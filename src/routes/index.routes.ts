60fe069f446d9f557bfe2e1c890f79423753ea6a
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-28 09:28:44 -0300

    feat: category register route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,5 +1,6 @@
 import { loginRouter } from "./login.routes";
 import { customerRouter } from "./customer.routes";
 import { productRouter } from "./product.routes";
+import { categoryRouter } from "./category.routes";
 
-export { customerRouter, loginRouter, productRouter };
+export { customerRouter, loginRouter, productRouter, categoryRouter };
