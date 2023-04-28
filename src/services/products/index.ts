51b1c33e3a97559ab567bfaf32a53ecf1e5cdebf
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-27 22:39:19 -0300

    feat: soft delete product add service

--- src/services/products/index.ts
+++ src/services/products/index.ts
@@ -1,5 +1,11 @@
 import { getProductsService } from "./get";
 import { registerProductService } from "./register";
+import { deactivateProductAddService } from "./softdelete";
 import { updateProductService } from "./update";
 
-export { registerProductService, updateProductService, getProductsService };
+export {
+  registerProductService,
+  updateProductService,
+  getProductsService,
+  deactivateProductAddService,
+};
