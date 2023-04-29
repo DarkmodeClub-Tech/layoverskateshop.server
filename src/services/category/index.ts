0b5212164a947d3779ee35a4a6461922a07c96c5
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-04-29 18:30:52 -0300

    feat: delete category service

--- src/services/category/index.ts
+++ src/services/category/index.ts
@@ -1,5 +1,11 @@
 import { getCategoriesService } from "./get";
 import { registerCategoryService } from "./register";
 import { updateCategoryService } from "./update";
+import { deleteCategoryService } from "./destroy";
 
-export { registerCategoryService, getCategoriesService, updateCategoryService };
+export {
+  registerCategoryService,
+  getCategoriesService,
+  updateCategoryService,
+  deleteCategoryService,
+};
