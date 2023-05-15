ed78e9fffade87732530e3ecece444d063eba73b
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-05-15 20:43:11 -0300

    feat: insert products shortcut

---
+++ src/services/cart/index.ts
@@ -0,0 +1,5 @@
+import { createCartService } from "./create";
+import { insertProductsCartService } from "./insertProducts";
+import { retrieveCartService } from "./retrieve";
+
+export { createCartService, retrieveCartService, insertProductsCartService };
