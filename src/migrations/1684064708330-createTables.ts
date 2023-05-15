40d7e24597ef714bd6d0ee195474b5f71d4f5934
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-05-15 20:47:15 -0300

    update migrations and database entities relations

---
+++ src/migrations/1684064708330-createTables.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTables1684064708330 implements MigrationInterface {
+    name = 'CreateTables1684064708330'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "carts_products_products" ("cartsId" uuid NOT NULL, "productsId" uuid NOT NULL, CONSTRAINT "PK_88c6c87a047a1483387693e891a" PRIMARY KEY ("cartsId", "productsId"))`);
+        await queryRunner.query(`CREATE INDEX "IDX_6d4a98ce8aefd303215e05d6c8" ON "carts_products_products" ("cartsId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_8689ca568058fafcbfc0fcd753" ON "carts_products_products" ("productsId") `);
+        await queryRunner.query(`ALTER TABLE "carts_products_products" ADD CONSTRAINT "FK_6d4a98ce8aefd303215e05d6c8c" FOREIGN KEY ("cartsId") REFERENCES "carts"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE "carts_products_products" ADD CONSTRAINT "FK_8689ca568058fafcbfc0fcd7539" FOREIGN KEY ("productsId") REFERENCES "products"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "carts_products_products" DROP CONSTRAINT "FK_8689ca568058fafcbfc0fcd7539"`);
+        await queryRunner.query(`ALTER TABLE "carts_products_products" DROP CONSTRAINT "FK_6d4a98ce8aefd303215e05d6c8c"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_8689ca568058fafcbfc0fcd753"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_6d4a98ce8aefd303215e05d6c8"`);
+        await queryRunner.query(`DROP TABLE "carts_products_products"`);
+    }
+
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,21 +1,9 @@
 import { InitialMigration1682346444041 } from "./1682346444041-initialMigration";
-import { CreateTables1682346648117 } from "./1682346648117-createTables";
-import { AlterTables1682346922430 } from "./1682346922430-alterTables";
-import { AlterTables1682347365164 } from "./1682347365164-alterTables";
-import { AlterTables1682348287338 } from "./1682348287338-alterTables";
-import { AlterTable1682448498485 } from "./1682448498485-alterTable";
-import { AlterTables1682459438455 } from "./1682459438455-alterTables";
-import { AlterTables1682464795384 } from "./1682464795384-alterTables";
-import { AlterTables1682467557649 } from "./1682467557649-alterTables";
+import { CreateTables1684064393378 } from "./1684064393378-createTables";
+import { CreateTables1684064708330 } from "./1684064708330-createTables";
 
 export {
   InitialMigration1682346444041,
-  CreateTables1682346648117,
-  AlterTables1682346922430,
-  AlterTables1682347365164,
-  AlterTables1682348287338,
-  AlterTable1682448498485,
-  AlterTables1682459438455,
-  AlterTables1682464795384,
-  AlterTables1682467557649,
+  CreateTables1684064393378,
+  CreateTables1684064708330,
 };
