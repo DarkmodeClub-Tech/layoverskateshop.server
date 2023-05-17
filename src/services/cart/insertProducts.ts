32741daaa578bc7b6a90c94e9c5e859de582f134
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-05-16 23:25:06 -0300

    update: create cart

--- src/services/cart/insertProducts.ts
+++ src/services/cart/insertProducts.ts
@@ -1,18 +1,22 @@
 import AppDataSource from "../../data-source";
-import { Cart, Product } from "../../entities";
+import { Cart, CartToProducts, Product } from "../../entities";
 import { retrieveCartService } from "../cart";
+import { createCartProductsService } from "../cartToProducts/create";
 import { retrieveProductService } from "../products";
 
 export const insertProductsCartService = async (
   cart_id: string,
   products_id: string[]
-): Promise<Cart> => {
+): Promise<Cart | CartToProducts> => {
   const cartRepo = AppDataSource.getRepository(Cart);
+  const cartTOProductsRepo = AppDataSource.getRepository(CartToProducts);
   const cart: Cart = await retrieveCartService(cart_id);
 
   products_id.map(async (product_id) => {
     let product = await retrieveProductService(product_id);
-    cart.products.push(product);
+
+    const cartToProduct = await createCartProductsService(cart, product);
+    cart.cart_to_product.push(cartToProduct);
     await cartRepo.save(cart);
   });
 
