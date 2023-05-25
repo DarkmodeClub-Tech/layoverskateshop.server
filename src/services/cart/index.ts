63c0699a8363689b15f71d21f08e7ba61f000877
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-05-25 19:53:52 -0300

    feat: remove product from cart service

--- src/services/cart/index.ts
+++ src/services/cart/index.ts
@@ -1,5 +1,11 @@
 import { createCartService } from "./create";
+import { removeProductsFromCartService } from "./removeProduct";
 import { insertProductsInCartService } from "./insertProducts";
 import { retrieveCartService } from "./retrieve";
 
-export { createCartService, retrieveCartService, insertProductsInCartService };
+export {
+  createCartService,
+  retrieveCartService,
+  insertProductsInCartService,
+  removeProductsFromCartService,
+};
