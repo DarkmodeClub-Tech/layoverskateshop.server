91ce3eb40637805584cc58a97f8cc74512c0627d
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-05-29 20:19:26 -0300

    feat: register seller service

--- src/services/seller/register.ts
+++ src/services/seller/register.ts
@@ -1,8 +1,13 @@
+import { hash } from "bcrypt";
 import AppDataSource from "../../data-source";
 import { Seller } from "../../entities";
 
 export const registerSellerService = async (data: Seller): Promise<Seller> => {
   const sellerRepo = AppDataSource.getRepository(Seller);
+
+  const { password } = data;
+
+  data.password = await hash(password, 10);
   const seller = sellerRepo.create(data);
 
   await sellerRepo.save(seller);
