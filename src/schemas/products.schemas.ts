8696f74bda15c85a581e8b333cc3107f898e1fbf
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-05-30 15:56:05 -0300

    update: add description field to register request product schema.

--- src/schemas/products.schemas.ts
+++ src/schemas/products.schemas.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const registerProductRequestSchema = z.object({
   title: z.string(),
   price: z.string(),
+  description: z.string(),
   max_installments: z.string(),
   stock_amount: z.string(),
   category: z.string(),
