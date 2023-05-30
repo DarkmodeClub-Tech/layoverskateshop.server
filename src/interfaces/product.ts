46dd780813ac1ea3f5e946bcae54618aaf8e08be
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-05-30 15:38:48 -0300

    update: register product interface

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -1,1 +1,9 @@
+import { z } from "zod";
+import { registerProductRequestSchema } from "../schemas/products.schemas";
+import { TPhoto } from "./photo";
+
 export type TProduct = { product_id: string; amount: number };
+
+export type TRegisterProductRequest = z.infer<
+  typeof registerProductRequestSchema
+>;
