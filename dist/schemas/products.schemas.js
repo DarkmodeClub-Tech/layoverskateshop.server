7853eb4d7071cb44e20e78866c5c13b6f6091521
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-06-21 20:47:55 -0300

    update: add columns available_colors and available_sizes

--- dist/schemas/products.schemas.js
+++ dist/schemas/products.schemas.js
@@ -9,4 +9,6 @@ exports.registerProductRequestSchema = zod_1.z.object({
     max_installments: zod_1.z.string(),
     stock_amount: zod_1.z.string(),
     category: zod_1.z.string(),
+    available_sizes: zod_1.z.array(zod_1.z.string()),
+    available_colors: zod_1.z.array(zod_1.z.string()),
 });
