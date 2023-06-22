ea495984f79f9759c1b25561e85abe158c2d78ad
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-06-21 22:03:56 -0300

    update: cart - create service, add columns requested sizes and requested_colors

--- src/services/cartProducts/create.ts
+++ src/services/cartProducts/create.ts
@@ -4,13 +4,17 @@ import { Cart, CartProduct, Product } from "../../entities";
 export const createCartProductsService = async (
   cart: Cart,
   product: Product,
-  amount: number
+  amount: number,
+  requested_colors: string[],
+  requested_sizes: string[]
 ): Promise<CartProduct> => {
   const cartProductRepo = AppDataSource.getRepository(CartProduct);
   const cartProduct = cartProductRepo.create({
     cart: cart,
     product: product,
     cart_amount: amount,
+    requested_colors: requested_colors,
+    requested_sizes: requested_sizes,
   });
 
   await cartProductRepo.save(cartProduct);
