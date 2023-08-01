ed5b459d70adadc3fbd0ae8c8b180e6dc666db38
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-08-01 20:41:29 -0300

    feat: fuzzySearch by query

--- src/services/products/get.ts
+++ src/services/products/get.ts
@@ -1,11 +1,29 @@
 import AppDataSource from "../../data-source";
 import { Product } from "../../entities";
+import { AppError } from "../../errors/appError";
+import Fuse from "fuse.js";
 
 export const getProductsService = async (
   offset: number,
-  limit: number
+  limit: number,
+  search?: string
 ): Promise<Product[]> => {
   const productRepo = AppDataSource.getRepository(Product);
   const products = await productRepo.find({ skip: offset, take: limit });
+
+  if (search) {
+    const fuseOptions = {
+      keys: ["title", "description"],
+    };
+
+    const fuse = new Fuse(products, fuseOptions);
+    const result = fuse.search(search);
+    return result.map((item) => item.item);
+
+    // const foundProducts = products.filter((p) =>
+    //   p.title.toLowerCase().includes(search.toLowerCase())
+    // );
+    // return foundProducts;
+  }
   return products;
 };
