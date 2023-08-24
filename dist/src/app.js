ff436d4f8207f0ce7f35d04d944defca14663610
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-08-24 09:36:51 -0300

    fix: set corsOrigins

--- dist/src/app.js
+++ dist/src/app.js
@@ -6,22 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 require("express-async-errors");
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./routes");
 const handleError_1 = __importDefault(require("./middlewares/handleError"));
 const cart_routes_1 = require("./routes/cart.routes");
 const app = (0, express_1.default)();
-const corsOptions = {
-    origin: [
-        "https://layoverskateshop.vercel.app",
-        "https://layoverskateshop.admview.vercel.app",
-        "https://layover-skateshop.onrender.com",
-        "http://localhost:3001",
-    ],
-};
-(0, cors_1.default)({});
+// const corsOptions = {
+//   origin: [
+//     "https://layoverskateshop.vercel.app",
+//     "https://layoverskateshop.admview.vercel.app",
+//     "https://layover-skateshop.onrender.com",
+//     "http://localhost:3001",
+//   ],
+// };
 app.use(express_1.default.json());
-app.use((0, cors_1.default)(corsOptions));
+// app.use(cors(corsOptions));
 app.use("/sellers", routes_1.sellerRouter);
 app.use("/customers", routes_1.customerRouter);
 app.use("/products", routes_1.productRouter);
